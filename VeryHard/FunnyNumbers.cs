e405ed29ddee1106b0d38d2c9df613c2196d79c7
JohanFrom <gouldsilver@gmail.com>
2023-01-10 09:12:49 +0100

    added funny numbers

---
+++ VeryHard/FunnyNumbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdabitChallenges.VeryHard
+{
+    // Funny Numbers: https://edabit.com/challenge/ixBrn5gukJR6aHJdp
+    public static class FunnyNumbers
+    {
+        public static int? FunnyNumbersCalc(int num, int divider)
+        {
+            char[] nums = num.ToString().ToCharArray();
+            double newNum = 0;
+            double power = divider;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                newNum += Math.Pow(double.Parse(nums[i].ToString()), power);
+                power++;
+            }
+
+            return num * divider == newNum ? divider : null;
+        }
+    }
+}
