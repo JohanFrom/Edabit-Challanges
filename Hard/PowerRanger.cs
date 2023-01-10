97937b7ce513245ec339e3e79135f6bf27956bf3
JohanFrom <gouldsilver@gmail.com>
2023-01-10 09:16:17 +0100

    added power ranger

---
+++ Hard/PowerRanger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdabitChallenges.Hard
+{
+    // Power Ranger: https://edabit.com/challenge/McGCFZYn8ikn3GSqz
+    public static class PowerRanger
+    {
+        public static int PowerRangerCalc(int power, int min, int max)
+        {
+            int count = 0;
+            for (int i = 1; i <= max; i++)
+            {
+                if (Math.Pow(i, power) >= min && Math.Pow(i, power) <= max)
+                {
+                    count++; 
+                }
+            }
+            return count;
+        }
+    }
+}
