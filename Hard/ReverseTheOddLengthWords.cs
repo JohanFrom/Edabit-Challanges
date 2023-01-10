001199ba83d1f2a1d92d83bab3a83ae92d2ca057
JohanFrom <gouldsilver@gmail.com>
2023-01-10 08:54:01 +0100

    added Reverse odd length word

---
+++ Hard/ReverseTheOddLengthWords.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdabitChallenges.Hard
+{
+    // Reverse the Odd Length Words: https://edabit.com/challenge/yYfQoo6rkMezauG77
+    public static class ReverseTheOddLengthWords
+    {
+        public static string ReverseOdd(string str)
+        {
+            if (str == string.Empty)
+            {
+                return string.Empty;
+            }
+
+            string[] splittedStr = str.Split(" ");
+
+            if (splittedStr.Length == 1)
+            {
+                return splittedStr[0].Length % 2 != 0 ? ReverseString(splittedStr[0]) : splittedStr[0];
+            }
+
+            for (int i = 0; i < splittedStr.Length; i++)
+            {
+                splittedStr[i] = splittedStr[i].Length % 2 != 0 ? ReverseString(splittedStr[i]) : splittedStr[i];
+            }
+
+            return string.Join(" ", splittedStr);
+        }
+
+        private static string ReverseString(string str)
+        {
+            char[] array = str.ToCharArray();
+            Array.Reverse(array);
+            return new string(array);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,18 +1,11 @@
-﻿using EdabitChallenges.Expert;
-namespace EdabitChallenges
+﻿namespace EdabitChallenges
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("All challenges are ordered in difficulty level.");
-            string[] hej = BuildingBinaryClock.BinaryClock("10:37:49");
-
-            for (int i = 0; i < hej.Length; i++)
-            {
-                Console.WriteLine(hej[i]);
-            }
-            
+            Console.WriteLine("To test the methods, just use the static class name.methodname");
         }
     }
 }
