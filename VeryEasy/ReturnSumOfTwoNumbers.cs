6abc5a6d7cbf25028343871e0cebd6b5a004d512
JohanFrom <gouldsilver@gmail.com>
2023-01-10 09:39:41 +0100

    added very easy

---
+++ VeryEasy/ReturnSumOfTwoNumbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdabitChallenges.VeryEasy
+{
+    // Return the Sum of Two Numbers: https://edabit.com/challenge/xfRucdwGksiyjZq4K
+
+    public static class ReturnSumOfTwoNumbers
+    {
+        public static int Sum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
