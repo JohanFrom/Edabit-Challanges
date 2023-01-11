17bbf35474ffafc2dd650fe5d6ed9a0854adafb2
JohanFrom <gouldsilver@gmail.com>
2023-01-11 15:37:14 +0100

    added is number palindrome?

---
+++ Medium/IsNumberPalindrome.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdabitChallenges.Medium
+{
+    // Check if a Number is a Palindrome: https://edabit.com/challenge/qrJyh8ouvr2iseZy4
+    public static class IsNumberPalindrome
+    {
+        public static bool IsPalindrome(int num) => string.Join("", num.ToString().ToCharArray().Reverse()) == num.ToString();
+    }
+}
