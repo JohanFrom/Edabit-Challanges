2c5e107741f300e5da932a6db5e39f0d24550741
JohanFrom <gouldsilver@gmail.com>
2023-01-11 08:57:44 +0100

    Added factorial challenge

---
+++ Easy/ReturnTheFactorial.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdabitChallenges.Easy
+{
+    // Return the Factorial: https://edabit.com/challenge/PmhP5mXMRrBoH3WoB
+    public static class ReturnTheFactorial
+    {
+        public static int Factorial(int num)
+        {
+            int fact = 1;
+            do
+            {
+                fact *= num;
+                num--;
+            } while (num > 0);
+            return fact;
+        }
+    }
+}
