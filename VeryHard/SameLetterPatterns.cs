f1a074cc843f2c8b96ec2fc71b30e1ba33221af9
JohanFrom <gouldsilver@gmail.com>
2023-01-16 08:17:33 +0100

    Added Same letter pattern

---
+++ VeryHard/SameLetterPatterns.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdabitChallenges.VeryHard
+{
+    // Same Letter Patterns: https://edabit.com/challenge/QpSMWmb8AoTdtrBQZ
+    public static class SameLetterPatterns
+    {
+        public static bool SameLetterPattern(string str1, string str2)
+        {
+            return MapString(str1) == MapString(str2);
+        }
+
+        private static string MapString(string input)
+        {
+            Dictionary<char, int> seen = new();
+            int index = 0;
+            return string.Join(string.Empty,
+              input.Select(c => seen.ContainsKey(c) ? seen[c] : seen[c] = index++));
+        }
+    }
+}
