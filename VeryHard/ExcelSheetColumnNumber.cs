4f1b80ee9ef3690b5ae67a1f0b6691facae3a985
JohanFrom <gouldsilver@gmail.com>
2023-01-16 08:33:22 +0100

    Added challenge

---
+++ VeryHard/ExcelSheetColumnNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdabitChallenges.VeryHard
+{
+    // Excel Sheet Column Number: https://edabit.com/challenge/Dq2etCDAtTrPe2Xq6
+    public static class ExcelSheetColumnNumber
+    {
+        public static int TitleToNumber(string str)
+        {
+            int value = 0;
+            foreach(var c in str)
+            {
+                value *= 26;
+                value += c - 'A' + 1;
+            }
+            return value;
+        }
+    }
+}
