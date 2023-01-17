5826c271c0ec15df0e855d97951a7582d50231f2
JohanFrom <gouldsilver@gmail.com>
2023-01-17 15:33:01 +0100

    Added string factoring

---
+++ Hard/StringFactoring.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdabitChallenges.Hard
+{
+    // String Factoring: https://edabit.com/challenge/wh9ZuSXZqnjTL69je
+    public static class StringFactoring
+    {
+        public static string StringFactor(int[] arr)
+        {
+            var groupNumbers = arr.GroupBy(x => x);
+            StringBuilder sb = new();
+
+            foreach (var nums in groupNumbers)
+                sb.Append(nums.Count() == 1 ? nums.Key + " x " : nums.Key + "^" + nums.Count() + " x ");
+
+            sb.Length -= 3;
+            return sb.ToString();   
+        }
+    }
+}
