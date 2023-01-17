ff1cdb624ebb3d24654e0b87d758298faf818012
JohanFrom <gouldsilver@gmail.com>
2023-01-17 10:38:58 +0100

    Added maskify the string

---
+++ Medium/MaskifyTheString.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdabitChallenges.Medium
+{
+    // Maskify the String: https://edabit.com/challenge/RFeBL2TzSf7mRMNJi
+    public static class MaskifyTheString
+    {
+        public static string Maskify(string str)
+        {
+            if(str == string.Empty)
+                return string.Empty;
+            
+            if(str.Length < 4)
+                return str;
+    
+            string visiblePart = str[^4..];
+            string hiddenPart = string.Concat(str[..^visiblePart.Length].ToCharArray().Select(x => x = '#').ToArray());
+            return hiddenPart + visiblePart;
+        }
+    }
+}
