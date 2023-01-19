b8a5e23306837a4a52a3125c99accb638be3ec88
JohanFrom <gouldsilver@gmail.com>
2023-01-19 13:13:08 +0100

    Added Password validation

---
+++ VeryHard/PasswordValidation.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdabitChallenges.VeryHard
+{
+    // Password Validation: https://edabit.com/challenge/etT7orqDDXJF2zGYM
+    public static class PasswordValidation
+    {
+        // Without Regex
+        public static bool ValidatePassword(string password)
+        {
+            // Length check
+            if (!(password.Length >= 6 && password.Length <= 24)) 
+                return false;
+
+            // Special character check
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (char.IsSymbol(password[i]))
+                {
+                    if (!_specialCharacters.Contains(password[i]))
+                        return false;
+                }
+            }
+            // Contains at least one digit, uppercase and lowercase
+            if (!password.Any(char.IsDigit) || !password.Any(char.IsUpper) || !password.Any(char.IsLower)) 
+                return false;
+            
+
+            // No repeating characters (case sensitive)
+            if(password.Where((c, i) => i >= 2 && password[i - 1] == c && password[i - 2] == c).Any())
+                return false;
+            
+
+            return true;
+        }
+
+        private static readonly HashSet<char> _specialCharacters = new()
+        {
+            '!',
+            '@',
+            '#',
+            '$',
+            '%',
+            '^',
+            '&',
+            '*',
+            '(',
+            ')',
+            '+',
+            '=',
+            '_',
+            '-',
+            '{',
+            '}',
+            '[',
+            ']',
+            ':',
+            ';',
+            '"',
+            '\'',
+            '?',
+            '<',
+            '>',
+            ',',
+            '.'
+        };
+    }
+}
