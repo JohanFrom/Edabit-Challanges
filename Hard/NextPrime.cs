b6d944281cd6e61ff99e42119f8838592b8bef97
JohanFrom <gouldsilver@gmail.com>
2023-01-19 08:25:05 +0100

    more challenges

---
+++ Hard/NextPrime.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdabitChallenges.Hard
+{
+    // Next Prime: https://edabit.com/challenge/FKb8JY75nkaHz7B3F
+    public static class NextPrime
+    {
+        public static int PrimeNext(int num)
+        {
+            if (num > 2 && num % 2 == 0)
+                ++num;
+
+            while (!IsPrime(num))
+            {
+                num += 2;
+            }
+
+            return num;
+        }
+
+        private static bool IsPrime(int num)
+        {
+            if (num <= 1 && num % 2 == 0)
+                return false;
+
+            if (num == 2)
+                return true;
+
+            double root = Math.Pow(num, 0.5);
+
+            for (int i = 3; i < root; i++)
+            {
+                if (num % i == 0)
+                    return false;
+            }
+
+            return true;
+
+        }
+    }
+}
---
+++ VeryHard/WorkingNineToFive.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdabitChallenges.VeryHard
+{
+    // Working 9 to 5: https://edabit.com/challenge/rkzH6YsPNgoJjn75i
+    public static class WorkingNineToFive
+    {
+        public static string OverTime(double[] arr)
+        {
+            double normalWorkedHours = Math.Min(arr[1], 17) - Math.Min(arr[0], 17);
+            double overTimeWorkedHours = Math.Max(arr[1], 17) - Math.Max(arr[0], 17);
+            double moneyCalculation = (normalWorkedHours * arr[2]) + (overTimeWorkedHours * arr[2] * arr[3]);
+
+            return (moneyCalculation % 1 == 0) switch
+            {
+                true => $"${moneyCalculation.ToString("N2").Replace(",", ".")}",
+                false => $"${moneyCalculation.ToString("N2").Replace(",", ".")}"
+            };
+            
+        }
+    }
+}
