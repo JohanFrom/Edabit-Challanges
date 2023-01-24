cee21052600799892cd89ffac81afeac42ce32c9
JohanFrom <gouldsilver@gmail.com>
2023-01-24 09:24:32 +0100

    Added two medium

---
+++ Medium/ArrayOfMultiples.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdabitChallenges.Medium
+{
+    // Array of Multiples: https://edabit.com/challenge/2QvnWexKoLfcJkSsc
+    public static class ArrayOfMultiples
+    {
+        public static int[] ArrayMultiplies(int num, int length)
+        {
+            StringBuilder sb = new();
+            for (int i = 1; i <= length; i++)
+            {
+                sb.Append(num * i + " ");
+            }
+
+            sb.Length -= 1;
+            return sb.ToString().Split(" ").Select(x => int.Parse(x)).ToArray();
+        }
+    }
+}
---
+++ Medium/ReverseTheCase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdabitChallenges.Medium
+{
+    // Reverse the Case: https://edabit.com/challenge/99oN5igrbXddAjHEL
+    public static class ReverseTheCase
+    {
+        public static string ReverseCase(string str) => string.Concat(str.Select(x => char.IsLower(x) ? char.ToUpper(x) : char.ToLower(x)));
+        
+    }
+}
