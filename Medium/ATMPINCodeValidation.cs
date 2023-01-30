ef00b69ff96fbe2c7adb9e43a4a0d9e3640caf4f
JohanFrom <gouldsilver@gmail.com>
2023-01-30 09:52:59 +0100

    added new challenge

---
+++ Medium/ATMPINCodeValidation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdabitChallenges.Medium
+{
+    // ATM PIN Code Validation: https://edabit.com/challenge/gBYEFXeD9G2JMZ9dD
+    public static class ATMPINCodeValidation
+    {
+        public static bool ValidatePIN(string pin)
+        {
+            if(pin.Length == 4 || pin.Length == 6 && pin != string.Empty)
+            {
+                for (int i = 0; i < pin.Length; i++)
+                {
+                    if (char.IsLetter(pin[i])) return false;
+                }
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
