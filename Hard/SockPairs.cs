5739e076a91575356d4813f45e57af0f2c7f1758
Johan From <gouldsilver@gmail.com>
2023-02-14 15:19:29 +0100

    added sock pairs challenge

---
+++ Hard/SockPairs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdabitChallenges.Hard
+{
+    // Sock Pairs: https://edabit.com/challenge/C6wN5vGodWvWL7ZaK
+    public static class SockPairs
+    {
+        public static int PairsOfSocks(string str) =>
+            str.ToCharArray()
+                .GroupBy(item => item)
+                .ToDictionary(item => item.Key, item => item.Count())
+                .Select(x => x.Value / 2).Sum();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,18 +5,16 @@ using EdabitChallenges.Hard;
 using EdabitChallenges.VeryHard;
 using EdabitChallenges.Expert;
 using System.Text;
-using System.Linq;
-using System.Diagnostics;
 
 namespace EdabitChallenges
 {
     internal class Program
-    { 
+    {
         static void Main(string[] args)
         {
             Console.WriteLine("All challenges are ordered in difficulty level.");
             Console.WriteLine("To test the methods, just use the static class name.methodname e.g. TheCollatzConjecture.Collatz()");
-            
+
             CountCompletedChallenges();
             SearchChallengeSolution("Return the");
         }
