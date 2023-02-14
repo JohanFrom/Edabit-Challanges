31e41eab10a5651b4983d04fce541845a0cce4cd
Johan From <gouldsilver@gmail.com>
2023-02-14 15:28:08 +0100

    updated challenge solution

--- Hard/ReversingBinaryString.cs
+++ Hard/ReversingBinaryString.cs
@@ -9,11 +9,7 @@ namespace EdabitChallenges.Hard
     // Reversing a Binary String: https://edabit.com/challenge/TgtrCxdZxjaNBKvk3
     public static class ReversingBinaryString
     {
-        public static int ReversedBinaryInteger(int num)
-        {
-            string binary = Convert.ToString(num, 2);
-            char[] array = binary.ToCharArray().Reverse().ToArray();
-            return Convert.ToInt32(new string(array), 2);
-        }
+        public static int ReversedBinaryInteger(int num) => Convert.ToInt32(new string(Convert.ToString(num, 2).Reverse().ToArray()), 2);
+        
     }
 }
