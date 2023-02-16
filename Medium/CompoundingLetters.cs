0e764a21f6853e57e3af282e5e33d65b70a456f4
Johan From <gouldsilver@gmail.com>
2023-02-16 14:51:13 +0100

    added two mre challenges

---
+++ Medium/CompoundingLetters.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdabitChallenges.Medium
+{
+    // Compounding Letters: https://edabit.com/challenge/djCa8yiv5kYPhvcAZ
+    public static class CompoundingLetters
+    {
+        public static string Accum(string str)
+        {
+            StringBuilder sb = new();
+            str = str.ToLower();
+            for (int i = 0; i < str.Length; i++)
+            {
+                sb.Append(char.ToUpper(str[i]));
+                sb.Append(new string(str[i], i) + "-");
+            }
+            sb.Length += -1;
+            return sb.ToString();
+            
+        }
+    }
+}
---
+++ Medium/SolveTheEquation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdabitChallenges.Medium
+{
+    // Solve the Equation: https://edabit.com/challenge/jJshPejQ5C44TPpSv
+    public static class SolveTheEquation
+    {
+        public static int Equation(string equation) => int.Parse(new DataTable().Compute(equation, null).ToString() ?? "0");
+    }
+}
