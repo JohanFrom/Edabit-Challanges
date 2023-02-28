cd7cfdfb8597870c8c6d17b7d20eb79e7d8c62da
Johan From <gouldsilver@gmail.com>
2023-02-28 16:37:12 +0100

    Added new medium challenge

---
+++ Medium/TheMuseumOfIncrediblyDULLThings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdabitChallenges.Medium
+{
+    // The Museum of Incredibly DULL Things: https://edabit.com/challenge/dgHXtSrgyWbJ3cXvL
+    public static class TheMuseumOfIncrediblyDULLThings
+    {
+        public static int[] RemoveSmallest(int[] arr)
+        {
+            if(arr.Length == 0) return Array.Empty<int>();
+            var list = arr.ToList();
+            list.RemoveAt(list.IndexOf(arr.Min()));
+            return list.ToArray();
+        }
+    }
+}
