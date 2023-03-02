732c52d75904853d6c099105b0c1f2292a51c398
Johan From <gouldsilver@gmail.com>
2023-03-02 16:55:37 +0100

    Added medium challenge

---
+++ Medium/FindTheLargestNumbersInAGroupOfArrays.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdabitChallenges.Medium
+{
+    // Find the Largest Numbers in a Group of Arrays: https://edabit.com/challenge/nermqxzovZbfFBC9X
+    public static class FindTheLargestNumbersInAGroupOfArrays
+    {
+        public static double[] FindLargest(double[][] values) => values.Select(x => x.Max()).ToArray();
+    }
+}
